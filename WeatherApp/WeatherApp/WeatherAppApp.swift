14cad245e6a236a8972c06b808c6ea52295e5304
ParasMahadev <er.paras89@gmail.com>
2024-10-02 00:21:02 -0500

    Initial Commit
    -Launch Screen set up
    -App icon set up

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Paras Navadiya on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
